527c02f6aa6121056a1140a5f5dd7ea7a03cb322
Syed Shahid <syedshahid1215@gmail.com>
2022-07-29 16:24:27 +0500

    add validation rule to get an order by  id route

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,9 +1,20 @@
 const { validator } = require('../utils/validate');
 const { apiValidationErrors } = require("../utils/apiHelpers")
 
-const validationMiddleWare = (validationRule) => {
+const validationMiddleWare = (validationRule, param = "body") => {
     return async (req, res, next) => {
-        await validator(req.body, validationRule, {}, (err, status) => {
+        if(param === "body") {
+
+            await validator(req.body, validationRule, {}, (err, status) => {
+                if (!status) {
+                    res.status(412).json(apiValidationErrors(err))
+                } else {
+                    next();
+                }
+            }).catch( err => console.log(err))
+        }
+
+        await validator(req.params, validationRule, {}, (err, status) => {
             if (!status) {
                 res.status(412).json(apiValidationErrors(err))
             } else {
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,7 +3,7 @@ const { addProduct, getAllProducts } = require("../controller/productController"
 const { validationMiddleWare } = require("../middleware/validationMiddleware");
 const { createProductValRule } = require("../utils/validateRules")
 
-router.get("/", validationMiddleWare(createProductValRule), getAllProducts)
-router.post("/add", addProduct)
+router.get("/", getAllProducts)
+router.post("/add", validationMiddleWare(createProductValRule), addProduct)
 
 module.exports = router;
