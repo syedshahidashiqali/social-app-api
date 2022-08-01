68b276e18461a380f1cc155d937e12586534f4f6
Syed Shahid <syedshahid1215@gmail.com>
2022-08-01 11:54:56 +0500

    add token middleware to all reviews routes

--- routes/reviewsRoutes.js
+++ routes/reviewsRoutes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router()
 
 const { addReview, getAllReviews, getAllReviewsOfProduct } = require("../controller/reviewsController")
+const { verifyToken: auth } = require("../middleware/authMiddleware")
 
-router.post("/add", addReview)
-router.get("/", getAllReviews)
-router.get("/product/:productId", getAllReviewsOfProduct)
+router.post("/add", auth, addReview)
+router.get("/", auth, getAllReviews)
+router.get("/product/:productId", auth, getAllReviewsOfProduct)
 
 module.exports = router;
