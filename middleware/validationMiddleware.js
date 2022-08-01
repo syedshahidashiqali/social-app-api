2e4f627b68a973d4aa8888afefb6e78fc0a0c61b
Syed Shahid <syedshahid1215@gmail.com>
2022-08-01 17:43:34 +0500

    modified validation middleware to use custom messages from parameter

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,9 +1,9 @@
 const { validator } = require('../utils/validate');
 const { apiValidationErrors } = require("../utils/apiHelpers")
 
-const validationMiddleWare = (validationRule, param = "body") => {
+const validationMiddleWare = (validationRule, param = "body", customErrMessages = {}) => {
     return async (req, res, next) => {
-        await validator(req[param], validationRule, {}, (err, status) => {
+        await validator(req[param], validationRule, customErrMessages, (err, status) => {
             if (!status) {
                 res.status(412).json(apiValidationErrors(err))
             } else {
