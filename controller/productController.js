d39b400e3a080a211376cc230d977d1d64c17247
Syed Shahid <syedshahid1215@gmail.com>
2022-08-01 11:43:53 +0500

    add route for getting a single product

--- controller/productController.js
+++ controller/productController.js
@@ -28,7 +28,18 @@ const getAllProducts = async (req, res) => {
     }
 }
 
+const getProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+
+        res.status(200).json(apiSuccessWithData("Product data", product))
+    } catch(err) {
+        res.status(500).json(apiError(err.message))
+    }
+}
+
 module.exports = {
     addProduct,
-    getAllProducts
+    getAllProducts,
+    getProduct
 }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-const { addProduct, getAllProducts } = require("../controller/productController");
+const { addProduct, getAllProducts, getProduct } = require("../controller/productController");
 const { validationMiddleWare } = require("../middleware/validationMiddleware");
 const { createProductValRule } = require("../utils/validateRules")
 
 router.get("/", getAllProducts)
+router.get("/product/:id", getProduct)
 router.post("/add", validationMiddleWare(createProductValRule), addProduct)
 
 module.exports = router;
